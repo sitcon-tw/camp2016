cbdb9cbe9d9a892898be8be4654f8e4ceeffd6d8
Elantris <j88107333@gmail.com>
2016-05-23 22:44:08 +0800

    change order of pages

--- src/scripts/partial/navbar.jsx
+++ src/scripts/partial/navbar.jsx
@@ -1,54 +1,53 @@
-
 var React = require('react');
 var ruler = require('./ruler.jsx');
 
 var barContent = [
-	{ page:"SITCON 夏令營", dest:"coverPage" },
-	{ page:"關於", dest:"aboutPage" },
-	{ page:"錄取", dest:"enrollPage" },
-	{ page:"報名", dest:"paywayPage" },
-	{ page:"課程", dest:"coursePage" },
-	{ page:"工作團隊", dest:"staffPage" },
-	{ page:"贊助", dest:"sponsorPage" }
+	{ page: "SITCON 夏令營", dest: "coverPage" },
+	{ page: "關於", dest: "aboutPage" },
+	{ page: "課程", dest: "coursePage" },
+	{ page: "報名", dest: "paywayPage" },
+	{ page: "錄取", dest: "enrollPage" },
+	{ page: "工作團隊", dest: "staffPage" },
+	{ page: "贊助", dest: "sponsorPage" },
 ];
 
 var BarResponsive = React.createClass({
-	componentDidMount: function(){
+	componentDidMount: function() {
 		// this.getDOMNode().addEventListener('click', this.open , false);
-		this.refs.barResponsive.addEventListener('click', this.open , false);
+		this.refs.barResponsive.addEventListener('click', this.open, false);
 	},
-	bindOpen: function(){
+	bindOpen: function() {
 		// this.getDOMNode().addEventListener('click', this.open , false);
-		this.refs.barResponsive.addEventListener('click', this.open , false);
-		window.removeEventListener('click', this.close , false);
+		this.refs.barResponsive.addEventListener('click', this.open, false);
+		window.removeEventListener('click', this.close, false);
 	},
-	bindClose: function(){
-		window.addEventListener('click', this.close , false);
+	bindClose: function() {
+		window.addEventListener('click', this.close, false);
 		// this.getDOMNode().removeEventListener('click', this.open , false);
-		this.refs.barResponsive.removeEventListener('click', this.open , false);
+		this.refs.barResponsive.removeEventListener('click', this.open, false);
 	},
-	open: function(){
-		if( ruler.getScrollY() < 
-				ruler.getTopY( document.getElementById('navbar') )-60
-				){
+	open: function() {
+		if (ruler.getScrollY() <
+			ruler.getTopY(document.getElementById('navbar')) - 60
+		) {
 			$('html,body').animate({
-				scrollTop: ruler.getTopY( document.getElementById('navbar') )-60
-			} , 1000);
+				scrollTop: ruler.getTopY(document.getElementById('navbar')) - 60
+			}, 1000);
 		}
-		$('.barButtonBlock').css('display','block');
+		$('.barButtonBlock').css('display', 'block');
 		var that = this;
-		setTimeout( function(){
+		setTimeout(function() {
 			that.bindClose();
 			that = null;
-		},500);
+		}, 500);
 	},
-	close: function(){
-		$('.barButtonBlock').css('display','none');
+	close: function() {
+		$('.barButtonBlock').css('display', 'none');
 		var that = this;
-		setTimeout( function(){
+		setTimeout(function() {
 			that.bindOpen();
 			that = null;
-		},500);
+		}, 500);
 	},
 	render: function() {
 		return (
@@ -60,20 +59,20 @@ var BarResponsive = React.createClass({
 });
 
 var BarButton = React.createClass({
-	scrollTo: function(){
+	scrollTo: function() {
 		var target = this.props.dest;
 		$('html,body').animate({
-			scrollTop: ruler.getTopY( document.getElementById(target) )
-		} , 1000);
+			scrollTop: ruler.getTopY(document.getElementById(target))
+		}, 1000);
 	},
-	render: function(){
-		if( this.props.now=="true" )
+	render: function() {
+		if (this.props.now == "true")
 			return (
 				<div className="barButton nowPage" onClick={this.scrollTo}>
 					{this.props.children}
 				</div>
 			);
-		else 
+		else
 			return (
 				<div className="barButton" onClick={this.scrollTo}>
 					{this.props.children}
@@ -83,41 +82,41 @@ var BarButton = React.createClass({
 });
 
 var barButtonScrollMixin = {
-	componentDidMount: function(){
+	componentDidMount: function() {
 		window.addEventListener('scroll', this.checkWhichPage, false);
 	},
-	componentWillUnmount: function(){
+	componentWillUnmount: function() {
 		window.removeEventListener('scroll', this.checkWhichPage, false);
 	}
 };
 
 var BarButtonBlock = React.createClass({
 	mixins: [barButtonScrollMixin],
-	getInitialState: function(){
+	getInitialState: function() {
 		return {
 			nowPage: ""
 		};
 	},
-	checkWhichPage: function(){
+	checkWhichPage: function() {
 		var btns = this.props.buttons;
 		var nowPage = " ";
-		for(var i=btns.length-1 ; i>0 ; --i){
-			if( ruler.haveReached( document.getElementById(btns[i].dest) ) ){
+		for (var i = btns.length - 1; i > 0; --i) {
+			if (ruler.haveReached(document.getElementById(btns[i].dest))) {
 				nowPage = btns[i].dest;
 				break;
 			}
 		}
-		if( nowPage == this.state.nowPage )
+		if (nowPage == this.state.nowPage)
 			return;
-		this.setState( { nowPage:nowPage } );
+		this.setState({ nowPage: nowPage });
 	},
-	render: function(){
+	render: function() {
 		var nowPage = this.state.nowPage;
-		var allBarButtons = this.props.buttons.map(function(button,cnt){
+		var allBarButtons = this.props.buttons.map(function(button, cnt) {
 			var now = "false";
-			if( button.dest == nowPage )
+			if (button.dest == nowPage)
 				now = "true";
-			if( cnt == 0 )
+			if (cnt == 0)
 				return (
 					<div className="barButtonSubBlock" key={cnt}>
 						<BarButton now={now} dest={button.dest}>
@@ -144,25 +143,25 @@ var BarButtonBlock = React.createClass({
 });
 
 var NavbarScrollMixin = {
-	componentDidMount: function(){
+	componentDidMount: function() {
 		window.addEventListener('scroll', this.checkFixing, false);
 	},
-	componentWillUnmount: function(){
+	componentWillUnmount: function() {
 		window.removeEventListener('scroll', this.checkFixing, false);
 	}
 };
 var NavbarResizeMixin = {
-	componentDidMount: function(){
+	componentDidMount: function() {
 		window.addEventListener('resize', this.checkFixing, false);
 	},
-	componentWillUnmount: function(){
+	componentWillUnmount: function() {
 		window.removeEventListener('resize', this.checkFixing, false);
 	}
 };
 
 var Navbar = React.createClass({
-	mixins: [NavbarScrollMixin , NavbarResizeMixin],
-	getDefaultProps: function(){
+	mixins: [NavbarScrollMixin, NavbarResizeMixin],
+	getDefaultProps: function() {
 		return {
 			abs: {
 				position: "absolute",
@@ -175,23 +174,22 @@ var Navbar = React.createClass({
 			anchor: document.getElementById('coverPage')
 		}
 	},
-	getInitialState: function(){
+	getInitialState: function() {
 		return {
 			mode: "abs"
 		};
 	},
-	checkFixing: function(){
-		if( ruler.getScrollY() >= 
-				ruler.getBottomY(this.props.anchor)-60 ){
-			if( this.state.mode != "fix" )
+	checkFixing: function() {
+		if (ruler.getScrollY() >=
+			ruler.getBottomY(this.props.anchor) - 60) {
+			if (this.state.mode != "fix")
 				this.setState({ mode: "fix" });
-		}
-		else{
-			if( this.state.mode != "abs" )
+		} else {
+			if (this.state.mode != "abs")
 				this.setState({ mode: "abs" });
 		}
 	},
-	render: function(){
+	render: function() {
 		return (
 			<div ref="navbar" className="navbar" style={ this.props[ this.state.mode ] }>
 				<BarResponsive />
