86d31624028581ebebde63bd58bfd0bc0448fd86
Elantris <j88107333@gmail.com>
2016-06-15 21:01:17 +0800

    update course features and enrolled names

--- src/scripts/component/courseFeature.jsx
+++ src/scripts/component/courseFeature.jsx
@@ -44,29 +44,13 @@ var CourseWorldCafe = React.createClass({
 					<img src="img/alert.png"></img>
 				</div>
 				<p>世界咖啡館 ( The World Café ) 是一種腦力激盪的討論程序，SITCON 夏令營以此為創意發想，參考並設計出「視界咖啡館」活動，邀請到各領域及社群前輩，將傳統座談會形式改以小組討論的樣貌呈現，並與學員分享工作經驗、學習過程、面對困難的解決方式或希望給予 SITCON 夏令營學員的建議，提出值得學員思考、了解的觀點。期望透過縮短講師及學員間的距離，鼓勵學員踴躍提問、參與，進而產生良好的雙向交流。</p>
+				<p><strong>唐鳳</strong>現任國發會開放資料諮詢委員、十二年國教課發會委員，為零時政府 (g0v) 萌典、vTaiwan.tw 等多項專案貢獻者，於蘋果電腦、牛津大學出版社、Socialtext 等機構兼任顧問。專業領域為互動空間設計、程式語言學、計算語言學。</p>
+				<p><strong>吳淩（lingling）</strong>HITCON GIRLS 創辦人，台灣科技大學碩士生，《Girls in Tech Taiwan》"40 Under 40 Women in Tech" 獲獎者，曾經到韓國、中國等資安演討會演講，研究領域是資安事件調查、程式安全與逆向工程。</p>
+				<p></p>
 			</div>
 		);
 	}
 });
-
-/*
-				<p>
-					<strong>Allen Own</strong>
-					翁浩正 (Allen Own)，戴夫寇爾 DEVCORE 執行長，台灣駭客年會 HITCON CMT 總召。專長駭客手法分析、滲透測試、資安專業訓練。
-				</p>
-				<p>
-					<strong>James Huang</strong>
-					是一個跳進社會科學領域無法自拔的程式人。
-				</p>
-				<p>
-					<strong>曾義峰</strong>
-					網路暱稱 ant。略懂「程式設計」、「資訊安全」及「智慧財產權」，目前專注於「合法的安全程式設計」。閒暇時投入自由 / 開放源碼軟體的開發與研究，每有會意，便欣然忘食。對於「經濟學」、「混沌複雜科學」亦有喜好。平時雖愛胡思亂想、天馬行空，卻又不斷盡力將夢想成真。
-				</p>
-				<p>
-					<strong>雨蒼</strong>
-					自由軟體工作者，公民記者，興趣為社會文化觀察。 目前任職於民間司法改革基金會。
-				</p>
-*/
 
 var CourseFeature = React.createClass({
 	render: function(){
--- src/scripts/component/enrollPage.jsx
+++ src/scripts/component/enrollPage.jsx
@@ -1,16 +1,20 @@
-
-
 /*var tmpContent = ["人生總充滿著不可思議，覺得不可能的事情也許下一秒就發生在眼前，即使如此我們還是會笑、會難過、會吵架，也會將努力灑落在夢想與希望的種子上。",
 "有些害羞內向的鎮長、和藹可親的副鎮長、喜歡小道消息的執行官、認真負責的訓練官，和其他好多好多的大家都是同為新手鎮努力的一份子，過程中也許有一點溫馨、有一點搞笑，還帶點苦澀，但這是我們的故事，要一起來嗎？"];
 */
 var enrolledName = [
-	// "洪Ｏ吟","蔡Ｏ名","張Ｏ傑","陳Ｏ均","謝Ｏ芳","吳Ｏ志","藍Ｏ彬","陳Ｏ霖","張Ｏ涵","陳Ｏ熙","李Ｏ廉","吳Ｏ翰","周Ｏ融","吳Ｏ丞","陳Ｏ立","林Ｏ滕","蕭Ｏ欣","陳Ｏ豪","許Ｏ維","高Ｏ哲","江Ｏ貞","劉Ｏ呈","羅Ｏ祐","方Ｏ元","蘇Ｏ嘉","吳Ｏ展","林Ｏ丞","干Ｏ軍","王Ｏ堯","鄧Ｏ擇","黃Ｏ寧","蔡Ｏ衡","柯Ｏ澤","李Ｏ元","黃Ｏ豪","廖Ｏ喆","嚴Ｏ峰","任Ｏ丞","張Ｏ成","周Ｏ宇","張Ｏ貴","楊Ｏ雨","闕Ｏ","施Ｏ維","朱Ｏ緯","廖Ｏ寬","郭Ｏ桓","李Ｏ陞","游Ｏ澔","李Ｏ穎","張Ｏ碩","林Ｏ廷","張Ｏ騰","郭Ｏ均","劉Ｏ平","張Ｏ懷","黃Ｏ雅","林Ｏ君","潘Ｏ霖","楊Ｏ辰","廖Ｏ誼","陳Ｏ","張Ｏ翔","張Ｏ瑞","彭Ｏ棱","蘇Ｏ安","李Ｏ陞","鄭Ｏ遠","隋Ｏ華","沈Ｏ哲","林Ｏ逸","羅Ｏ齊","白Ｏ婷","林Ｏ翔","陳Ｏ安","林Ｏ鴻","莊Ｏ伊","陳Ｏ霖","吳Ｏ軒","蘇Ｏ揚","張Ｏ芝","李Ｏ安","SeＯn","郭Ｏ平","陳Ｏ儒","劉Ｏ祥","李Ｏ哲","彭Ｏ倫","許Ｏ甄","邱Ｏ一","林Ｏ彥","蘇Ｏ嘉","廖Ｏ涵","許Ｏ瑋","張Ｏ菖","王Ｏ堯"
+	['呂Ｏ浩', '蘇Ｏ文', '許Ｏ慈', '林Ｏ岸', '張Ｏ語', '林Ｏ培', '張Ｏ宸', '張Ｏ', 'Peter Chang', '胡Ｏ芳', '盧Ｏ均', '黃Ｏ傑', '湯Ｏ摯', '姚Ｏ辰', '吳Ｏ嘉', '劉Ｏ函', '蔡Ｏ霖', '陳Ｏ銓', '盧Ｏ達', '鄭Ｏ卉', '王Ｏ澤', '林Ｏ倫', '陳Ｏ心', '陳Ｏ樺', '鍾Ｏ瑋', '鄭Ｏ謙', '蔡Ｏ夆', '蔡Ｏ哲', '詹Ｏ詠', '康Ｏ浩', '葉Ｏ多', '梁Ｏ東', '陳Ｏ阜', '鄭Ｏ尹', '林Ｏ瀚', '姜Ｏ妤', '李Ｏ庭', '余Ｏ序', '許Ｏ彬', '黃Ｏ維', '吳Ｏ達', '林Ｏ豐', '李Ｏ瑀', '戴Ｏ儀', '朱Ｏ廣', '汪Ｏ亮', '林Ｏ', ],
+	['張Ｏ喬', '藍Ｏ瑾', '楊Ｏ菱', 'PN Wu', '于Ｏ', ],
 ];
-var enrolledNameTable = [];
-for(var i=0,tmp=[] ; i<enrolledName.length ; i+=5){
-	enrolledNameTable.push(
-		enrolledName.slice( i , i+5 )
-	);
+var enrolledNameTable = [
+	[],
+	[],
+];
+for (var i in enrolledName) {
+	for (var j = 0; j < enrolledName[i].length; j += 5) {
+		enrolledNameTable[i].push(
+			enrolledName[i].slice(j, j + 5)
+		)
+	}
 }
 
 var React = require('react');
@@ -19,47 +23,36 @@ var ruler = require('../partial/ruler.jsx');
 var Table = require('../partial/table.jsx');
 
 var EnrollPage = React.createClass({
-	getDefaultProps: function(){
+	getDefaultProps: function() {
 		return { calledAnimation: false };
 	},
-	componentDidMount: function(){
+	componentDidMount: function() {
 		window.addEventListener('scroll', this.checkReached, false);
 	},
-	checkReached: function(){
-		if( !ruler.haveReached( document.getElementById('enrollPage') ) )
+	checkReached: function() {
+		if (!ruler.haveReached(document.getElementById('enrollPage')))
 			return;
-		if( this.props.calledAnimation )
+		if (this.props.calledAnimation)
 			return;
 		this.props.calledAnimation = true;
 		window.removeEventListener('scroll', this.checkReached, false);
 		enrollPageAnimation();
 	},
-	render: function(){
-		/*
-
-						<div className="tmpContent">
-							<strong>{tmpContent[0]}</strong>
-							<strong>{tmpContent[1]}</strong>
-							<strong className="claimDate">
-								<p>公告錄取</p>
-								<p>06 . 05</p>
-							</strong>
-						</div>
-
-		*/
-		/*
-						<Table applyClass="enrollPageTable" colClass="popInPre">
-							{enrolledNameTable}
-						</Table>
-		*/
+	render: function() {
 		return (
 			<div className="enrollPage">
 				<div className="pageContainer">
 					<div className="left">
 						<h2>錄取團員</h2>
-						<div className="tmpContent" style={{textAlign: 'center'}}>
-							近期公開
-						</div>
+						{/* <div className="tmpContent" style={{textAlign: 'center'}}>近期公開</div>*/}
+						<h3>正取</h3>
+						<Table applyClass="enrollPageTable" colClass="popInPre">
+							{enrolledNameTable[0]}
+						</Table>
+						<h3>備取 <small className="hint">若正取學員未在期限前成功繳費，我們將會依後補順序寄信通知候補學員</small></h3>
+						<Table applyClass="enrollPageTable" colClass="popInPre">
+							{enrolledNameTable[1]}
+						</Table>
 						<img className="girl" src={"img/girl.png"} />
 					</div>
 				</div>
@@ -73,14 +66,14 @@ ReactDOM.render(
 	document.getElementById('enrollPage')
 );
 
-function enrollPageAnimation(){
+function enrollPageAnimation() {
 	var popIn = document.querySelectorAll('#enrollPage .col');
-	for(var i=0 ; i<popIn.length ; ++i){
-		popIn[i].className = popIn[i].className + 
-			" delayRand" + 
-			( 1+Math.floor((Math.random()*19)) );
+	for (var i = 0; i < popIn.length; ++i) {
+		popIn[i].className = popIn[i].className +
+			" delayRand" +
+			(1 + Math.floor((Math.random() * 19)));
 	}
-	for(var i=0 ; i<popIn.length ; ++i)
+	for (var i = 0; i < popIn.length; ++i)
 		popIn[i].className = popIn[i].className + " popIn";
 	popIn = null;
 }
